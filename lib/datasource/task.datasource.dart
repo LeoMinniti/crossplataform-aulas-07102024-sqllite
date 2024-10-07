7df8a603feb5aee0a738dfb92436de67ab06def8
leonardo.candido <leonsenai@gmail.com>
2024-10-07 20:55:43 -0300

    * Criando os metodos na datasource para usar os metodos do banco de dados

--- lib/datasource/task.datasource.dart
+++ lib/datasource/task.datasource.dart
@@ -1,7 +1,12 @@
+import 'package:get/get.dart';
+import 'package:rm76172/database/task.database.dart';
+import 'package:rm76172/domain/task.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TaskDataSource {
   late SharedPreferences _preference;
+  final TaskDatabase _database = TaskDatabase.instance; //criando variavel apra instanciar a conexao com o BD
+  final Rx<List<Task>?> _tasks = Rx(null); //variavel privada para refletir na view(tela)
 
   initPreference() async {
     _preference = await SharedPreferences.getInstance();
@@ -14,4 +19,13 @@ class TaskDataSource {
   save(String key, String value){
     _preference.setString(key, value);
   }
+
+  saveTask(Task task){
+    _database.insert(task);
+  }
+
+  loadTasks() async {
+    _tasks.value = await _database.readAll();
+  }
+
 }
